9d4855f4ade50143d9b7ec8140362fe4e7639664
Jun Park <pj2417@gmail.com>
2025-03-31 18:55:21 +0900

    add clip comment

--- vae/ae_train.py
+++ vae/ae_train.py
@@ -109,7 +109,9 @@ if __name__ == "__main__":
     def train_step(samples: Tensor) -> Tensor:
         opt.zero_grad()
         output, _ = model(X_train[samples])
-        loss = output.clip(1e-7, 1 - 1e-7).binary_crossentropy(X_train[samples])
+        loss = output.clip(1e-7, 1 - 1e-7).binary_crossentropy(
+            X_train[samples]
+        )  # add clip for numerical stability
         loss.backward()
         opt.step()
         return loss.realize()
